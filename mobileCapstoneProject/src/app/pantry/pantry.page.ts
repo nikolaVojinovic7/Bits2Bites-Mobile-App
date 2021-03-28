eedb586c7aee43210487fe2d9b5c74339393d39d
CDinatale <cora.dinatale@gmail.com>
2021-03-28 17:28:31 -0400

    added ingredient drop down lists to each category

--- mobileCapstoneProject/src/app/pantry/pantry.page.ts
+++ mobileCapstoneProject/src/app/pantry/pantry.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class PantryPage {
 
+  showDairy = false;
+  showVegetables = false;
+  showFruits = false;
+  showGrains = false;
+  showMeat = false;
+  showSeafood = false;
+  showSpices = false;
+  showSweeteners = false;
+  showNuts = false;
+
+  toggleDairy() {
+    this.showDairy = !this.showDairy;
+ }
+ toggleVegetables() {
+  this.showVegetables = !this.showVegetables;
+}
+toggleFruits() {
+  this.showFruits = !this.showFruits;
+}
+toggleGrains() {
+  this.showGrains = !this.showGrains;
+}
+toggleMeat() {
+  this.showMeat = !this.showMeat;
+}
+toggleSeafood() {
+  this.showSeafood = !this.showSeafood;
+}
+toggleSpices() {
+  this.showSpices = !this.showSpices;
+}
+toggleSweeteners() {
+  this.showSweeteners = !this.showSweeteners;
+}
+toggleNuts() {
+  this.showNuts = !this.showNuts;
+}
+
   constructor() {}
 
 }
