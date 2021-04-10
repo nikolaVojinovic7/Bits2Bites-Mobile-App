884e6a8d0fcad2aeaed34cd7599c7804f0101185
stefangrujic <gruicstefan@gmail.com>
2021-04-10 05:28:00 -0400

    The back end for the app, got it woking in tab2

--- mobileCapstoneProject/src/app/tab2/tab2.page.ts
+++ mobileCapstoneProject/src/app/tab2/tab2.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+    results: any;
+    url = 'http://localhost:8087/api/public/allUsers';
 
-  constructor() {}
+    getUserByName: any;
+    urlName = 'http://localhost:8087/api/public/users/username/Stefan';
 
+    constructor(private http: HttpClient) {}
+
+    getUsers() {
+
+    }
+
+    ngOnInit(): void {
+      this.results = this.http.get(this.url).pipe(
+        map(results => {
+          console.log('RAW :: ', results);
+          return results;
+        })
+      );
+    }
 }
