c1e8635c081b9ad49e2745aa854c6b9e2e022914
esogelola <esogelola@gmail.com>
2021-04-11 20:26:11 -0400

    Modified UserController to allow CORS for port 4200, Finished User registration

--- mobileCapstoneProject/src/app/data/user.service.ts
+++ mobileCapstoneProject/src/app/data/user.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   readonly ROOT_URL = 'http://localhost:8087/api/public';
 
   constructor(private http: HttpClient) {}
 
-  getAllUsers(){
-    return this.http.get(this.ROOT_URL + '/allUsers')
+  getAllUsers() {
+    return this.http.get(this.ROOT_URL + '/allUsers');
   }
 
-  createUser(form){
-    return this.http.post(this.ROOT_URL + '/createUser', form.value)
+  createUser(form) {
+    return this.http.post(this.ROOT_URL + '/createUser', form);
+  }
+
+  loginUser(email, password) {
+    return this.http.get(`${this.ROOT_URL}/login/${email}&${password}`);
   }
 }
